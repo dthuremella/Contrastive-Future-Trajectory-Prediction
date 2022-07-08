f9752835d8b328209759d62b239a75ee67695e58
Divya Thuremella <dthuremella@robots.ox.ac.uk>
2022-07-08 09:59:52 +0100

    new file bmc_loss.py

---
+++ bmc_loss.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.loss import _Loss
+from torch.distributions import MultivariateNormal as MVN
+
+def bmc_loss_md(pred, target, noise_var):
+    """Compute the Multidimensional Balanced MSE Loss (BMC) between `pred` and the ground truth `targets`.
+    Args:
+      pred: A float tensor of size [batch, d].
+      target: A float tensor of size [batch, d].
+      noise_var: A float number or tensor.
+    Returns:
+      loss: A float tensor. Balanced MSE Loss.
+    """
+    I = torch.eye(pred.shape[-1])
+    logits = MVN(pred.unsqueeze(1), noise_var*I).log_prob(target.unsqueeze(0))  # logit size: [batch, batch]
+    loss = F.cross_entropy(logits, torch.arange(pred.shape[0]))     # contrastive-like loss
+    loss = loss * (2 * noise_var).detach()  # optional: restore the loss scale, 'detach' when noise is learnable 
+    
+    return loss
+
+class BMCLoss(_Loss):
+    def __init__(self, init_noise_sigma):
+        super(BMCLoss, self).__init__()
+        self.noise_sigma = torch.nn.Parameter(torch.tensor(init_noise_sigma, device="cuda"))
+
+    def forward(self, pred, target):
+        noise_var = self.noise_sigma ** 2
+        loss = bmc_loss_md(pred, target, noise_var)
+        return loss
