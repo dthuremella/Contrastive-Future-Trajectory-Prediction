81608bb6236f949b9649e7b2052eb5971072d836
Divya Thuremella <dthuremella@robots.ox.ac.uk>
2022-07-08 10:26:57 +0100

    device of new tensor

--- bmc_loss.py
+++ bmc_loss.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
 from torch.distributions import MultivariateNormal as MVN
 
-def bmc_loss_md(pred, target, noise_var):
+def bmc_loss_md(y, labels, noise_var):
     """Compute the Multidimensional Balanced MSE Loss (BMC) between `pred` and the ground truth `targets`.
     Args:
       pred: A float tensor of size [batch, d].
@@ -12,6 +12,12 @@ def bmc_loss_md(pred, target, noise_var):
     Returns:
       loss: A float tensor. Balanced MSE Loss.
     """
+    # y has shape (bs, 20, 12 ,2)
+    # labels has shape (bs, 12, 2)
+    pred = torch.flatten(y, start_dim=1)
+    target = torch.stack([labels for i in range(20)], dim=1)  # (bs, 20, 12, 2)
+    target = torch.flatten(target, start_dim=1)
+
     I = torch.eye(pred.shape[-1])
     logits = MVN(pred.unsqueeze(1), noise_var*I).log_prob(target.unsqueeze(0))  # logit size: [batch, batch]
     loss = F.cross_entropy(logits, torch.arange(pred.shape[0]))     # contrastive-like loss
@@ -20,9 +26,9 @@ def bmc_loss_md(pred, target, noise_var):
     return loss
 
 class BMCLoss(_Loss):
-    def __init__(self, init_noise_sigma):
+    def __init__(self, init_noise_sigma, device):
         super(BMCLoss, self).__init__()
-        self.noise_sigma = torch.nn.Parameter(torch.tensor(init_noise_sigma, device="cuda"))
+        self.noise_sigma = torch.nn.Parameter(torch.tensor(init_noise_sigma, device=device))
 
     def forward(self, pred, target):
         noise_var = self.noise_sigma ** 2
